5c4c259b4209a9f65289482ff02092ad22850a9a
erikeis <eise.05@A321-15>
2022-06-14 11:57:37 +0200

    Úpravy výpisu

--- Prevody/Prevod.cs
+++ Prevody/Prevod.cs
@@ -179,49 +179,49 @@ namespace Prevody
                 switch (hex[i])
                 {
                     case '1':
-                        bin += "0001";
+                        bin += "0001 ";
                         break;
                     case '2':
-                        bin += "0010";
+                        bin += "0010 ";
                         break;
                     case '3':
-                        bin += "0011";
+                        bin += "0011 ";
                         break;
                     case '4':
-                        bin += "0100";
+                        bin += "0100 ";
                         break;
                     case '5':
-                        bin += "0101";
+                        bin += "0101 ";
                         break;
                     case '6':
-                        bin += "0110";
+                        bin += "0110 ";
                         break;
                     case '7':
-                        bin += "0111";
+                        bin += "0111 ";
                         break;
                     case '8':
-                        bin += "1000";
+                        bin += "1000 ";
                         break;
                     case '9':
-                        bin += "1001";
+                        bin += "1001 ";
                         break;
                     case 'A':
-                        bin += "1010";
+                        bin += "1010 ";
                         break;
                     case 'B':
-                        bin += "1011";
+                        bin += "1011 ";
                         break;
                     case 'C':
-                        bin += "1100";
+                        bin += "1100 ";
                         break;
                     case 'D':
-                        bin += "1101";
+                        bin += "1101 ";
                         break;
                     case 'E':
-                        bin += "1110";
+                        bin += "1110 ";
                         break;
                     case 'F':
-                        bin += "1111";
+                        bin += "1111 ";
                         break;
                     default:
                         break;
